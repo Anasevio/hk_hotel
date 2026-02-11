7b12606e82108ac2f453ddc899ed781b2104d82d
Lyass <ilyas.nhb@gmail.com>
2026-02-11 21:29:52 +0700

    Mengubah form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>Login</title>
-        <!-- font -->
-        <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
-            rel="stylesheet"
-        />
-        <!-- css -->
-        <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
-        <!-- icon -->
-        <script src="https://unpkg.com/feather-icons"></script>
-    </head>
-    <body>
-        <form id="loginForm">
-            @csrf
-            <div class="login-wrapper">
-                <div class="logo-area">
-                    <img src="{{ asset('images/Logo SIG.png') }}" />
-                    <img src="{{ asset('images/LOGO PH.png') }}" />
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <!-- css -->
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <!-- icon -->
+       <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <div class="login-wrapper">
+        <div class="logo-area">
+            <img src="{{ asset('images/Logo SIG.png') }}">
+            <img src="{{ asset('images/LOGO PH.png') }}">
+        </div>
 
-                <div class="login-card">
-                    <h2>LOGIN</h2>
-                    <p>Please Sign In to continue.</p>
-                    <img src="{{ asset('images/verified.png') }}" />
+
+        <div class="login-card">
+            <h1>LOGIN</h1>
+            <p>Please Sign In to continue.</p> 
+            <img src="{{ asset('images/verified.png') }}">
 
                     <div class="input-group">
                         <i data-feather="user"></i>
